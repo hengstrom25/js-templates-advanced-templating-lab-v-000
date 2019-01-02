033a91a15602b3e6a28a971141b05f7db4970966
Heidi Engstrom <hengstrom25@gmail.com>
2019-01-02 16:39:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function init() {
   //put any page initialization/handlebars initialization here
   
-}
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial').innerHTML);
+  Handlebars.registerHelper('displayIngredient', function(ingredient) {
+    return new Handlebars.SafeString('<li name="ingredients">' + ingredient + '<li>');
+  });
+  
+  let formTemplate = document.getElementById("recipe-form-template").innerHTML;
+  let formTemplateFn = Handlebars.compile(formTemplate);
+  document.getElementById('main').innerHTML = formTemplateFn({ingredients: ['', '', '', '', '']});
+}  
+  
+  
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
